11a36b91684ae6072185e2d7c3413030e4d8340a
Yasser Bigdeli <44984561+yasser64b@users.noreply.github.com>
2021-07-20 10:11:49 -0700

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,16 @@
+def selection_sort(list_a):
+    indexing_length=range(0,len(list_a)-1)
+    for i in indexing_length:
+        min_value=i
+            
+        for j in range(i+1, len(list_a)):
+            if list_a[j]<list_a[min_value]:
+                min_value=j
+
+        if min_value !=i: 
+            list_a[min_value], list_a[i]= list_a[i],list_a[min_value] # must change simultaniously
+            # this can give you wrong answer
+            #              list_a[min_value]= list_a[i] 
+            #              list_a[i]=list_a[min_value]
+
+    return list_a 
