019883f0a5ce6400f9fa50cc76ca77ec1340529c
Yasser Bigdeli <yasse.bigdeli@gmail.com>
2021-07-20 11:12:41 -0700

    Create quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,20 @@
+def quick_sort(sequence):
+    length=len(sequence)
+    if length <=1:
+        return sequence
+    else:
+        pivot=sequence.pop()
+
+    items_greater=[]
+    items_lower=[]
+
+    for item in sequence:
+        if item>pivot:
+            items_greater.append(item)
+        else:
+            items_lower.append(item)
+        
+    return quick_sort(items_lower)+[pivot]+quick_sort(items_greater)
+
+print(quick_sort([1,2,5,63,58,32,5,3,8,25,5,3]))   
+
