eae9d59d7983da40e62fc5fe5c729fffaade3028
Yasser Bigdeli <yasse.bigdeli@gmail.com>
2021-07-20 10:48:49 -0700

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,18 @@
+def insertion_sort(list_a):
+
+    indexing_length=range(1,len(list_a))
+    for i in indexing_length:
+        value_to_sort=list_a[i]
+
+        while list_a[i-1]>value_to_sort and i>0:
+            list_a[i], list_a[i-1]=list_a[i-1],list_a[i]
+            i-=1
+
+    
+    return list_a
+
+
+print(insertion_sort([1,5,3,4,5,6,7,8,90,3]))
+
+
+
