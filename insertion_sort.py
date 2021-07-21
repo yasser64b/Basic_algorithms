66d66bd146f867796d4cb2f0c5c590e2083df152
Yasser Bigdeli <yasse.bigdeli@gmail.com>
2021-07-20 23:31:05 -0700

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,18 +1,13 @@
 def insertion_sort(list_a):
-    ''' this function sorts a list using insertion algorithm'''
+    ''' this function sorts a list using insertion algorithm _ complexity: n*log(n)'''
     indexing_length=range(1,len(list_a))
     for i in indexing_length:
         value_to_sort=list_a[i]
-
         while list_a[i-1]>value_to_sort and i>0:
             list_a[i], list_a[i-1]=list_a[i-1],list_a[i]
             i-=1
-
-    
     return list_a
 
-
-print(insertion_sort([1,5,3,4,5,6,7,8,90,3]))
-
+print(insertion_sort([1,5,3,4,5,6,7,8,90,3,0,1,-1]))
 
 
